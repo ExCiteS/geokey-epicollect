7de1ebb7ecef2579c9a62e1dc3d0b4b19d46a8eb
Oliver Roick <oliver.roick@gmail.com>
2014-06-26 11:32:05 +0100

    Added comments

--- serializer.py
+++ serializer.py
@@ -1,13 +1,23 @@
+"""
+Serialises a Community Maps into EcML, an XML dialiect describing forms for
+EpiCollect's mobile app.
+"""
 from lxml import etree
 
 
 class ProjectFormSerializer(object):
     def create_label(self, field_name):
+        """
+        Creates a `<label>` element for a field
+        """
         label = etree.Element('label')
         label.text = field_name
         return label
 
     def create_item(self, label, value):
+        """
+        Creates a `<item>` element. To used with select fields.
+        """
         item = etree.Element('item')
         item.append(self.create_label(label))
 
@@ -18,6 +28,11 @@ class ProjectFormSerializer(object):
         return item
 
     def create_base_input(self, field):
+        """
+        Creates a basic `<input>` element for text, number and date inputs.
+        Additional attributes can be added in the respective serializer
+        methods.
+        """
         return etree.Element(
             'input',
             ref=field.key,
@@ -25,6 +40,11 @@ class ProjectFormSerializer(object):
         )
 
     def create_base_select1(self, field):
+        """
+        Creates a basic `<select1>` element for lookups and true/false fields.
+        Additional attributes can be added in the respective serializer
+        methods.
+        """
         return etree.Element(
             'select1',
             ref=field.key,
@@ -32,12 +52,18 @@ class ProjectFormSerializer(object):
         )
 
     def serialize_textfield(self, field):
+        """
+        Serialises a TextField.
+        """
         element = self.create_base_input(field)
         element.append(self.create_label(field.name))
 
         return element
 
     def serialize_numericfield(self, field):
+        """
+        Serialises a NumericField.
+        """
         element = self.create_base_input(field)
         element.attrib['decimal'] = 'true'
 
@@ -51,6 +77,9 @@ class ProjectFormSerializer(object):
         return element
 
     def serialize_truefalse_field(self, field):
+        """
+        Serialises a TrueFalseField.
+        """
         element = self.create_base_select1(field)
         element.append(self.create_label(field.name))
         element.append(self.create_item('True', 'true'))
@@ -59,6 +88,9 @@ class ProjectFormSerializer(object):
         return element
 
     def serialize_singlelookup_field(self, field):
+        """
+        Serialises a LookupField.
+        """
         element = self.create_base_select1(field)
         element.append(self.create_label(field.name))
 
@@ -68,6 +100,9 @@ class ProjectFormSerializer(object):
         return element
 
     def serialize_datetime_field(self, field):
+        """
+        Serialises a DateTimeField.
+        """
         element = self.create_base_input(field)
         element.attrib['date'] = 'dd/MM/yyyy'
 
