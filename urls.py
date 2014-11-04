abc4025e5a9f93e70dc488256022fcb65a5caac9
Oliver Roick <oliver.roick@gmail.com>
2014-11-04 15:49:42 +0000

    Fixed URL pattern

--- urls.py
+++ urls.py
@@ -7,7 +7,7 @@ from views import (
 urlpatterns = patterns(
     '',
     url(
-        r'^projects/(?P<project_id>[0-9]+)/$',
+        r'^projects/(?P<project_id>[0-9]+).xml$',
         EpiCollectProject.as_view(),
         name='project_form'),
     url(
