30fe8f159e474dcca9ec01fe079db0c870c598e8
Oliver Roick <oliver.roick@gmail.com>
2015-02-16 12:50:58 +0000

    Release 0.1.0-beta-2

--- setup.py
+++ setup.py
@@ -10,14 +10,14 @@ setup(
     description='Enables GeoKey to be used with EpiCollect for data collection.',
 
     # Version number (initial):
-    version="0.1.0-beta",
+    version="0.1.0-beta-2",
 
     # Application author details:
     author="Oliver Roick",
     author_email="o.roick@ucl.ac.uk",
 
     url = 'https://github.com/ExCiteS/geokey-epicollect', # use the URL to the github repo
-    download_url = 'https://github.com/ExCiteS/geokey-epicollect/tarball/0.1-beta',
+    download_url = 'https://github.com/ExCiteS/geokey-epicollect/tarball/0.1-beta-2',
 
     # Packages
     packages=["geokey_epicollect"],
